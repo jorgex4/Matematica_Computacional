fe5029878f4434a11f8b0b3e5b2f5dba144f653c
jorgex4 <jorjeluisandrade300@gmail.com>
2024-10-02 19:39:16 -0500

    Se agrega al proyecto

---
+++ Hola_mundo.py
@@ -0,0 +1,14 @@
+print("Hola mundo")
+print("Hola mundo")
+print("Hola mundo")
+print("Hola mundo")
+print("Hola mundo")
+print("Hola mundo")
+print("Hola mundo")
+print("Hola mundo")
+print("Hola mundo")
+print("Hola mundo")
+a=23
+b=60
+c= a+ b
+print(c)
